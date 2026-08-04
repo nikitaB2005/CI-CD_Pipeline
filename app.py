4c16314d5357e95103675dc9a98feeac964be8c5
nikitaB2005 <nikita233@lpu.in>
2026-08-04 10:36:43 +0530

    Updated

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 import json
+import socket
 
 app = Flask(__name__)
+hostname = socket.gethostname()
 
 @app.route("/")
 def dashboard():
@@ -26,4 +28,4 @@ def health():
     }
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
